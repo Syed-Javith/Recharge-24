fe7df57fcea61358a49edb14e25e71450c37c575
Syed-Javith <syedjavith14@gmail.com>
2024-01-24 23:22:12 +0530

    Component created

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -7,7 +7,7 @@ const page: FC<pageProps> = async ({}) => {
   return (
     <div className="flex justify-center">
       <div className="max-w-[400px]">
-<RegisterForm/>
+        <RegisterForm/>
       </div>
       </div>
   );
