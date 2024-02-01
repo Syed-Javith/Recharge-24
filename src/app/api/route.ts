2efdf119889aa3694d379716a425f9e21f2a880c
krishnamurthy <krishna101ravi@gmail.com>
2024-02-01 11:16:14 +0530

    debug error

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,11 +1,33 @@
 import { getAuthSession } from "@/lib/auth";
+import { getAccessJwtSecretKey } from "@/lib/utils";
+
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
-  const session = getAuthSession();
+  const session = await getAuthSession();
+  const token = getAccessJwtSecretKey();
   if (session) {
-    return NextResponse.json({ message: session }, { status: 200 });
+    const { id, first_name, last_name } = session;
+    if (id === "") {
+      return NextResponse.json(
+        { message: { session: { id: "Not found" }, token: token } },
+        { status: 200 }
+      );
+    } else {
+      return NextResponse.json(
+        {
+          message: {
+            session: { id: id, first_name: first_name, last_name: last_name },
+            token: token,
+          },
+        },
+        { status: 200 }
+      );
+    }
   } else {
-    return NextResponse.json({ message: "No session" }, { status: 200 });
+    return NextResponse.json(
+      { message: { session: "No session", token: token } },
+      { status: 200 }
+    );
   }
 }
