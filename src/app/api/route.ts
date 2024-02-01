085cfc67acdf703c0e31ca9cb44fbbd474ab88d7
krishnamurthy <krishna101ravi@gmail.com>
2024-02-01 10:58:52 +0530

    check session endpoint

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,5 +1,11 @@
+import { getAuthSession } from "@/lib/auth";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
-  return NextResponse.json({ message: "Hello WORLD" }, { status: 200 });
+  const session = getAuthSession();
+  if (session) {
+    return NextResponse.json({ message: session }, { status: 200 });
+  } else {
+    return NextResponse.json({ message: "No session" }, { status: 200 });
+  }
 }
