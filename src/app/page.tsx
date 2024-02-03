9786ac7d19b000c4a2ab843af0d6290ceb4fc131
Subhikshaa23 <210701264@rajalakshmi.edu.in>
2024-02-03 20:07:34 +0530

    Add faq and footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,9 @@ import AboutRecharge from "@/components/Home/AboutRecharge";
 import AboutRec from "@/components/Home/AboutRec";
 // import Gallery from '@/components/gallery/GallerySlider';
 import GallerySection from '@/components/gallery/Gallery';
+import FAQ from '@/components/Home/Faq';
+import Footer from '@/components/Home/Footer';
+
 export default function Home() {
   return (
     <div className="overflow-x-hidden recharge " style={{backgroundColor: "#020617"}}>
@@ -11,6 +14,8 @@ export default function Home() {
       <AboutRecharge/>
       <AboutRec/>
       <GallerySection/>
+      <FAQ/>
+      <Footer/>
       {/* <div className="h-[200px] w-[100%] bg-white"></div> */}
     </div>
   );
