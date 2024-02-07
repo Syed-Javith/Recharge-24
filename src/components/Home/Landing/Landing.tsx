d48e28a3ec0e9566d69e821059760abd8e30ca79
Santhosh <dev.santhoshp@gmail.com>
2024-02-07 22:12:13 +0530

    done css modules for landing section in homepage

--- src/components/Home/Landing/Landing.tsx
+++ src/components/Home/Landing/Landing.tsx
@@ -1,13 +1,13 @@
 "use client"
 import React, { useEffect } from 'react'
 import Link from 'next/link'
-import './Landing.css'
+import styles from './Landing.module.css'
 
 
 const Landing = () => {
     useEffect(() => {
         function rotateRectangles() {
-            let rectangle: NodeList = document.querySelectorAll('.fin');
+            const rectangle: NodeList = document.querySelectorAll('.'+styles.fin);
             rectangle.forEach(function (rect: Node) {
                 if (rect instanceof HTMLElement) {
                     const myElement: HTMLElement = rect;
@@ -17,69 +17,72 @@ const Landing = () => {
         }
 
         function hoverRotate() {
-            var rotateButton = document.getElementById('front-btn');
-            var leftElements = document.querySelectorAll('.fin-l');
-            var rightElements = document.querySelectorAll('.fin-r');
-            rotateButton?.addEventListener('mouseenter', function () {
+            const rotateButton = document.getElementsByClassName(styles.front_btn);
+            const leftElements = document.querySelectorAll('.'+styles.fin_l);
+            const rightElements = document.querySelectorAll('.'+styles.fin_r);
+            rotateButton[0]?.addEventListener('mouseenter', function () {
                 leftElements.forEach(function (element) {
-                    element.classList.add('rotate-l');
+                    element.classList.add(styles.rotate_l);
                 });
                 rightElements.forEach(function (element) {
-                    element.classList.add('rotate-r');
+                    element.classList.add(styles.rotate_r);
                 });
             });
 
-            rotateButton?.addEventListener('mouseleave', function () {
+            rotateButton[0]?.addEventListener('mouseleave', function () {
                 leftElements.forEach(function (element) {
-                    element.classList.remove('rotate-l');
+                    element.classList.remove(styles.rotate_l);
                 });
                 rightElements.forEach(function (element) {
-                    element.classList.remove('rotate-r');
+                    element.classList.remove(styles.rotate_r);
                 });
             });
         }
-
-        console.log("DOne")
         rotateRectangles();
         hoverRotate()
     })
     return (
-        <main id='landing-main group'>
-            <div id="content">
-                <img src="/Landing/logo1.png" alt="Recharge Logo" id="logo" />
-                <img src="/Landing/eye.png" alt="Eye" id="eye" />
-                <div id="fin-row-1">
-                    <img src="/Landing/fin1.png" className="fin fin-1 fin-l" />
-                    <img src="/Landing/fin2.png" className="fin fin-1 fin-r" />
+        <main className={styles.landing_main}>
+            <div className={styles.content}>
+                <img src="/Landing/logo1.png" alt="Recharge Logo" className={styles.landing_logo} />
+                <img src="/Landing/eye.png" alt="Eye" className={styles.eye} />
+                <div className={styles.fin_row_1}>
+                    <img src="/Landing/fin1.png" className={`${styles.fin} ${styles.fin_1} ${styles.fin_l}`} />
+                    <img src="/Landing/fin2.png" className={`${styles.fin} ${styles.fin_1} ${styles.fin_r}`} />
                 </div>
-                <div id="fin-row-2">
-                    <img src="/Landing/fin1.png" className="fin fin-2 fin-l" />
-                    <img src="/Landing/fin2.png" className="fin fin-2 fin-r" />
+                <div className={styles.fin_row_2}>
+                    <img src="/Landing/fin1.png" className={`${styles.fin} ${styles.fin_2} ${styles.fin_l}`} />
+                    <img src="/Landing/fin2.png" className={`${styles.fin} ${styles.fin_2} ${styles.fin_r}`} />
                 </div>
-                <div id="fin-row-3">
-                    <img src="/Landing/fin1.png" className="fin fin-3 fin-l" />
-                    <img src="/Landing/fin2.png" className="fin fin-3 fin-r" />
+                <div className={styles.fin_row_3}>
+                    <img src="/Landing/fin1.png" className={`${styles.fin} ${styles.fin_3} ${styles.fin_l}`} />
+                    <img src="/Landing/fin2.png" className={`${styles.fin} ${styles.fin_3} ${styles.fin_r}`} />
                 </div>
-                <div id="fin-row-4">
-                    <img src="/Landing/fin1.png" className="fin fin-4 fin-l" />
-                    <img src="/Landing/fin2.png" className="fin fin-4 fin-r" />
-                    <h2 id="caption">Recharge Your Spirit And Reignite Your Passion!</h2>
-                    <h2 id="date">30 | 40 | 41 April</h2>
+                <div className={styles.fin_row_4}>
+                    <img src="/Landing/fin1.png" className={`${styles.fin} ${styles.fin_4} ${styles.fin_l}`} />
+                    <img src="/Landing/fin2.png" className={`${styles.fin} ${styles.fin_4} ${styles.fin_r}`} />
+                    <h2 className={styles.caption}>Recharge Your Spirit And Reignite Your Passion!</h2>
+                    <h2 className={styles.date}>30 | 40 | 41 April</h2>
                 </div>
-                <div id="fin-row-5">
-                    <img src="/Landing/fin1.png" className="fin fin-5 fin-l" />
-                    <img src="/Landing/fin2.png" className="fin fin-5 fin-r" />
-                    <button className="btn" id="front-btn"><Link href={'/proshow'}>BUY PROSHOW TICKETS</Link></button>
-                    <button className="btn" id="bg-btn">BUY PROSHOW TICKETS</button>
-                    <h3 id='landing-h3'>
+                <div className={styles.fin_row_5}>
+                    <img src="/Landing/fin1.png" className={`${styles.fin} ${styles.fin_5} ${styles.fin_l}`} />
+                    <img src="/Landing/fin2.png" className={`${styles.fin} ${styles.fin_5} ${styles.fin_r}`} />
+
+                    <button className={`${styles.btn} ${styles.front_btn}`}>
+                        <Link href={'/proshow'}>BUY PROSHOW TICKETS</Link>
+                    </button>
+                    <button className={`${styles.btn} ${styles.bg_btn}`}>BUY PROSHOW TICKETS</button>
+
+                    <h3 className={styles.landing_h3}>
                         <Link href='/event'>
                             Explore All Events {'>'}
                         </Link>
                     </h3>
+
                 </div>
             </div>
-            <img src="/Landing/light.png" alt="light" className="light" id="light-1" />
-            <img src="/Landing/light.png" alt="light" className="light" id="light-2" />
+            <img src="/Landing/light.png" alt="light" className={`${styles.light} ${styles.light_1}`} />
+            <img src="/Landing/light.png" alt="light" className={`${styles.light} ${styles.light_2}`} />
         </main>
     )
 }
