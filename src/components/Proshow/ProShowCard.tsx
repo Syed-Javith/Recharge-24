f0e25a06fe51a72c37dbbad5e2fae0d34cac1330
Syed-Javith <syedjavith14@gmail.com>
2024-02-07 08:40:07 +0530

    Proshow event listener

--- src/components/Proshow/ProShowCard.tsx
+++ src/components/Proshow/ProShowCard.tsx
@@ -23,13 +23,23 @@ const ProShowCard: FC<ProshowcardProps> = ({
 }) => {
   const [isButton, setIsButton] = useState<string>("visible");
   return (
-    <div className="h-200 w-72 p-3" onMouseLeave={() => setIsButton("visible")} onMouseOver={() => setIsButton("hidden")}>
+    <div className="h-200 w-72 p-3"
+      onMouseLeave={() => setIsButton("visible")}
+      onMouseOver={() => setIsButton("hidden")}
+      onTouchCancel={() => setIsButton("visible")}
+      onTouchStart={() => setIsButton("hidden")}
+      onClick={() => setIsButton((prev) => {
+        if (prev === "visible")
+          return "hidden"
+        return "visible"
+      })}
+    >
       <Card className="group relative cursor-pointer items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30">
         <div className="group relative cursor-pointer items-center  justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30">
           <div className="h-200 w-72">
             <img
               className="h-full w-full object-cover transition-transform duration-500 group-hover:rotate-3 group-hover:scale-125"
-              src='https://images.pexels.com/photos/674010/pexels-photo-674010.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'
+              src={proshow.image}
               alt=""
             />
           </div>
@@ -37,10 +47,10 @@ const ProShowCard: FC<ProshowcardProps> = ({
 
         <div className="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70">
           <div className="absolute inset-0 flex translate-y-[55%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0 pt-3 ">
-          <CardTitle className="font-dmserif relative bottom-0 left-0 mb-2 ml-2 title">
-  <span className="fast-flicker uppercase text-md mb-2 ">{proshow.name}</span>
-  <h4 className="date mt-3" >23 MARCH</h4>
-</CardTitle>
+            <CardTitle className="font-dmserif relative bottom-0 left-0 mb-2 ml-2 title">
+              <span className="fast-flicker uppercase text-md mb-2 ">{proshow.name}</span>
+              <h4 className="date mt-3" >23 MARCH</h4>
+            </CardTitle>
 
             <div className={"sm:hidden " + isButton}>
               <CardFooter>
@@ -66,7 +76,7 @@ const ProShowCard: FC<ProshowcardProps> = ({
                     disabled={
                       (proshow.is_registered &&
                         !proshow.premium &&
-                        !proshow.combo) || (datePremium[proshow.day-1]==1 || isPremiumCombo || isStandardCombo) 
+                        !proshow.combo) || (datePremium[proshow.day - 1] == 1 || isPremiumCombo || isStandardCombo)
                     }
                     is_registered={proshow.is_registered}
                     label="standard"
@@ -82,7 +92,7 @@ const ProShowCard: FC<ProshowcardProps> = ({
                     disabled={
                       (proshow.is_registered &&
                         !proshow.premium &&
-                        proshow.combo)  || (isPremiumCombo || isDayPremium)
+                        proshow.combo) || (isPremiumCombo || isDayPremium)
                     }
                     is_registered={proshow.is_registered}
                     label="standard combo"
@@ -107,12 +117,12 @@ const ProShowCard: FC<ProshowcardProps> = ({
             <CardDescription className="mb-3 text-md italic text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100">
               {proshow.description?.slice(0, 120)}{" "}
             </CardDescription>
-            <span className="bg-gradient-to-r from-pink-500 to-yellow-500 text-transparent text-lg bg-clip-text">Unlock Proshow for just Rs.{proshow.amount}</span> 
+            <span className="bg-gradient-to-r from-pink-500 to-yellow-500 text-transparent text-lg bg-clip-text">Unlock Proshow for just Rs.{proshow.amount}</span>
 
             <div className="rounded-full  px-3.5 py-2 font-com text-sm capitalize text-white ">
-       
-         
-          
+
+
+
 
               <CardFooter>
                 {/* 
@@ -120,7 +130,7 @@ const ProShowCard: FC<ProshowcardProps> = ({
       Applies for both REC and Non-REC
     */}
                 {proshow.premium && !proshow.combo && (
-                  
+
                   <BuyProShowButton
                     disabled={proshow.is_registered || isPremiumCombo}
                     label="premium"
@@ -137,7 +147,7 @@ const ProShowCard: FC<ProshowcardProps> = ({
                     disabled={
                       (proshow.is_registered &&
                         !proshow.premium &&
-                        !proshow.combo) || (datePremium[proshow.day-1]==1 || isPremiumCombo || isStandardCombo) 
+                        !proshow.combo) || (datePremium[proshow.day - 1] == 1 || isPremiumCombo || isStandardCombo)
                     }
                     label="standard"
                     proshowid={proshow.id}
@@ -153,7 +163,7 @@ const ProShowCard: FC<ProshowcardProps> = ({
                     disabled={
                       (proshow.is_registered &&
                         !proshow.premium &&
-                        proshow.combo)  || (isPremiumCombo || isDayPremium)
+                        proshow.combo) || (isPremiumCombo || isDayPremium)
                     }
                     label="standard combo"
                     proshowid={proshow.id}
