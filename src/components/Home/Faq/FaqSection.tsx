337644f058ca56093527def1bd45c10d3f12b27f
Subhikshaa23 <210701264@rajalakshmi.edu.in>
2024-02-11 23:32:45 +0530

    Footer altered

--- src/components/Home/Faq/FaqSection.tsx
+++ src/components/Home/Faq/FaqSection.tsx
@@ -9,7 +9,7 @@ interface FaqSectionProps {
 }
 
 const titleFont = localFont({ src: '../../../../public/fonts/Jura.ttf' })
-const subtitleFont = localFont({ src: '../../../../public/fonts/Jura.ttf' })
+const subtitleFont = localFont({ src: '../../../../public/fonts/chakra.ttf' })
 
 const FaqSection: React.FC<FaqSectionProps> = ({ question, answer }) => {
   const [isOpen, setIsOpen] = useState(false);
@@ -27,7 +27,7 @@ const FaqSection: React.FC<FaqSectionProps> = ({ question, answer }) => {
         </div>
       </div>
       <div className={`transition-all duration-300 ${isOpen ? 'max-h-96' : 'max-h-0'} overflow-hidden`}>
-        <div className={`${subtitleFont.className} bg-gray-900 p-4`}>{answer}</div>
+        <div className={`${titleFont.className} bg-gray-900 p-4`}>{answer}</div>
       </div>
     </div>
   );
