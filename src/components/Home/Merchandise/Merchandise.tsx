6c821f1df536d86741f2abec5fbb08f990d6f7cc
Syed-Javith <syedjavith14@gmail.com>
2024-02-12 19:41:12 +0530

    Jonita image changed, button changed as fanpit rather premium

--- src/components/Home/Merchandise/Merchandise.tsx
+++ src/components/Home/Merchandise/Merchandise.tsx
@@ -9,12 +9,11 @@ const Merchandise = () => {
     return (
         <div className='md:w-[70%] w-[80vh] m-auto min-h-[50vh]'>
             <div className="flex md:flex-row flex-col container">
-                <div className='px-4'>
+                <div className='px-4 md:w-full w-[50%]'>
                     <Neon text='MERCHANDISE' />
                     <p className={`${SketchFont.className} text-md`}>
                         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Debitis, 
-                        <br />
-                        mollitia beatae quaerat quisquam officiis voluptates quos id sed facilis vitae totam ut eos exercitationem. Cumque incidunt quas quod placeat non!
+\                        mollitia beatae quaerat quisquam officiis voluptates quos id sed facilis vitae totam ut eos exercitationem. Cumque incidunt quas quod placeat non!
                     </p>
                     <Link href={'/merchandise'}>
                         <Button className='mt-8'>
