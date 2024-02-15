d41db33e0fc3778a30a45e379a4d52250d19c5a1
Syed-Javith <syedjavith14@gmail.com>
2024-02-15 22:39:52 +0530

    Images fixed

--- src/components/gallery/Gallery.tsx
+++ src/components/gallery/Gallery.tsx
@@ -6,7 +6,7 @@ const ImageData = [
     name: "No Name",
     // url: "jonita.jpeg"
     // url: "new_jonita.webp"
-    url: "new_jonita_crop.JPG"
+    url: "jonita_final.jpg"
   },
   {
     name: "No Name",
@@ -24,7 +24,7 @@ const ImageData = [
     name: "No Name",
     // url: "sunburn.jpeg"
     // url: "new_sunburn.webp"
-    url: "new_sunburn_crop.JPG"
+    url: "sun.jpg"
   },
   {
     name: "No Name",
