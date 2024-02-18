2c9b596edd20674a0b0d403e8303daad7b28f8a6
Syed-Javith <syedjavith14@gmail.com>
2024-02-18 16:50:52 +0530

    Before merging,pull from master

--- src/components/profile/EventTicket.tsx
+++ src/components/profile/EventTicket.tsx
@@ -4,6 +4,19 @@ import Style from "./profile.module.css";
 import "./profile.module.css";
 
 const EventTicket = ({ eventDetail }: { eventDetail: EventDetailSchema }) => {
+    function convertTo12HourFormat(time: string ) {
+        var timeArray = time.split(":");
+        var hours = parseInt(timeArray[0]);
+        var minutes = timeArray[1];
+        var period = hours >= 12 ? "p.m" : "a.m";
+    
+        hours = hours > 12 ? hours - 12 : hours;
+        hours = hours == 0 ? 12 : hours;
+    
+        var convertedTime = hours + ":" + minutes + " " + period;
+    
+        return convertedTime;
+      }
     return(
        <>
             <div className={`${Style.profileEventCard}`}>
@@ -26,7 +39,7 @@ const EventTicket = ({ eventDetail }: { eventDetail: EventDetailSchema }) => {
                 <div className={`${Style.row} ${Style.row1}`}>
                 <div className={`${Style.item}`}>
                     <span className={`${Style.bigText}`}>ON</span>
-                    <span className={`${Style.regularText}`}>{ "Day: " + eventDetail.day + " " + eventDetail.time_of_event }</span>
+                    <span className={`${Style.regularText}`}>{ "Day: " + eventDetail.day + " " +  convertTo12HourFormat(eventDetail.time_of_event as string) }</span>
                 </div>
                 <div className={`${Style.item}`}>
                     <span className={`${Style.bigText}`}>AT</span>
